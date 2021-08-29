ed1b6d0ea80daa15dcb478476a2d69923cda8945
rishikaran0207 <sendittomemrk@gmail.com>
2021-08-29 13:22:38 +0530

    Added: Quotent_Remainder

--- src/main/java/com/Bridgelabz/BasicPrograms.java
+++ src/main/java/com/Bridgelabz/BasicPrograms.java
@@ -1,22 +1,16 @@
 package com.Bridgelabz;
 import java.util.Scanner;
-
     public class BasicPrograms {
         public static int heads=0,tails=0;
         public static void main(String[] args) {
             Scanner x =new Scanner(System.in);
-            System.out.println("Enter a Year to check: ");
-            int year =x.nextInt();
-            if(year < 1000 || year > 9999)
-                System.out.println("\nYou entered wrong year");
-            else
-            {
-                if(year % 4 == 0)
-                    System.out.println("\nEntered year is Leap Year");
-                else
-                    System.out.println("\nEntered year is not a Leap year");
-            }
-
+            System.out.println("Enter a number: ");
+            int num =x.nextInt();
+            System.out.println("Enter the divisor: ");
+            int div =x.nextInt();
+            int quotient=num / div;
+            int remainder=num % div;
+            System.out.println("The Quotient of the number: "+quotient);
+            System.out.println("The Remainder of the number:"+remainder);
         }
-
     }
