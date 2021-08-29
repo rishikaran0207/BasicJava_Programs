1b030853b60be33845f61ca8c9e4d83404541f8c
rishikaran0207 <sendittomemrk@gmail.com>
2021-08-29 14:07:14 +0530

    Added: Largestof3

--- src/main/java/com/Bridgelabz/BasicPrograms.java
+++ src/main/java/com/Bridgelabz/BasicPrograms.java
@@ -3,11 +3,18 @@ import java.util.Scanner;
     public class BasicPrograms {
         public static void main(String[] args) {
             Scanner x =new Scanner(System.in);
-            System.out.println("Enter an alphabet to ckeck vowels or consonant:");
-            char alp =x.next().charAt(0);
-            if(alp == 'a' || alp == 'e' || alp == 'i' || alp == 'o' || alp == 'u')
-                System.out.println("The character "+ alp +"  is a vowel");
+            System.out.println("Enter three numbers to find greatest--");
+            System.out.println("Enter the first number");
+            int n1= x.nextInt();
+            System.out.println("Enter the Second number");
+            int n2= x.nextInt();
+            System.out.println("Enter the third number");
+            int n3= x.nextInt();
+            if(n1>n2 && n1>n3)
+                System.out.println("The Number "+ n1 +"  is Largest among Three number");
+            else if(n2>n3)
+                System.out.println("The Number "+ n2 +"  is Largest among Three number");
             else
-                System.out.println("The character "+ alp +"  is an Consonant");
+                System.out.println("The Number "+ n3 +"  is Largest among Three number");
         }
     }
