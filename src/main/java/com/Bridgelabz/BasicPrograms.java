610ef836b417627f547ed0b33a5222e40d368cba
rishikaran0207 <sendittomemrk@gmail.com>
2021-08-29 13:13:24 +0530

    Added: Factors

--- src/main/java/com/Bridgelabz/BasicPrograms.java
+++ src/main/java/com/Bridgelabz/BasicPrograms.java
@@ -4,19 +4,18 @@ import java.util.Scanner;
     public class BasicPrograms {
         public static int heads=0,tails=0;
         public static void main(String[] args) {
-            Scanner x =new Scanner(System.in);
-            System.out.println("Enter a Year to check: ");
-            int year =x.nextInt();
-            if(year < 1000 || year > 9999)
-                System.out.println("\nYou entered wrong year");
-            else
+            Scanner y =new Scanner(System.in);
+            System.out.println("Enter a number: ");
+            int num =y.nextInt();
+            System.out.println("the factor is ");
+            for (int i=2;i<num;i++)
             {
-                if(year % 4 == 0)
-                    System.out.println("\nEntered year is Leap Year");
-                else
-                    System.out.println("\nEntered year is not a Leap year");
+                while(num%i==0) {
+                    System.out.println(i);
+                    num = num / i;
+                }
             }
-
+            if (num>2)
+                System.out.println(num);
         }
-
     }
