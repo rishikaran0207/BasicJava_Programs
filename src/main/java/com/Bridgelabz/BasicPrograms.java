a006e710c3f54cdda89a53ef3363b80b2a89c2d7
rishikaran0207 <sendittomemrk@gmail.com>
2021-08-29 13:53:38 +0530

    Added: Even&Odd

--- src/main/java/com/Bridgelabz/BasicPrograms.java
+++ src/main/java/com/Bridgelabz/BasicPrograms.java
@@ -1,21 +1,13 @@
 package com.Bridgelabz;
-import java.util.Scanner;git
+import java.util.Scanner;
     public class BasicPrograms {
-        public static int heads=0,tails=0;
         public static void main(String[] args) {
             Scanner x =new Scanner(System.in);
-            System.out.println("Enter a Year to check: ");
-            int year =x.nextInt();
-            if(year < 1000 || year > 9999)
-                System.out.println("\nYou entered wrong year");
+            System.out.println("Enter the number to find it is even or odd");
+            int num1 =x.nextInt();
+            if(num1 % 2 == 0)
+                System.out.println("The number "+num1+"  is Even");
             else
-            {
-                if(year % 4 == 0)
-                    System.out.println("\nEntered year is Leap Year");
-                else
-                    System.out.println("\nEntered year is not a Leap year");
-            }
-
+                System.out.println("The number "+num1+"  is Odd");
         }
-
     }
