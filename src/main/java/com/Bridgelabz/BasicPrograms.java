db85b65b2b975ed225399649b64679a14ddd2c98
rishikaran0207 <sendittomemrk@gmail.com>
2021-08-29 13:35:52 +0530

    Added: Swap

--- src/main/java/com/Bridgelabz/BasicPrograms.java
+++ src/main/java/com/Bridgelabz/BasicPrograms.java
@@ -1,21 +1,19 @@
 package com.Bridgelabz;
-import java.util.Scanner;git
-    public class BasicPrograms {
-        public static int heads=0,tails=0;
-        public static void main(String[] args) {
+import java.util.Scanner;
+public class BasicPrograms {
+    public static void main(String[] args) {
             Scanner x =new Scanner(System.in);
-            System.out.println("Enter a Year to check: ");
-            int year =x.nextInt();
-            if(year < 1000 || year > 9999)
-                System.out.println("\nYou entered wrong year");
-            else
-            {
-                if(year % 4 == 0)
-                    System.out.println("\nEntered year is Leap Year");
-                else
-                    System.out.println("\nEntered year is not a Leap year");
-            }
-
+            System.out.println("Enter the two number to swap: ");
+            System.out.println("Enter the 1st number:");
+            int num1 =x.nextInt();
+            System.out.println("Enter the 2nd number:");
+            int num2 =x.nextInt();
+            int temp;
+            temp=num1;
+            num1=num2;
+            num2=temp;
+            System.out.println("After swapping 1st number is "+num1);
+            System.out.println("After swapping 2nd number is "+num2);
         }
-
     }
+
